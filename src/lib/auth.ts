75731b219e84c85f2dca1787a34c3773d395d943
SPCMSK <izaurietamatiasignacio@gmail.com>
2026-05-07 16:04:05 -0400

    Fix: login3

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,6 +10,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID ?? "",
       clientSecret: process.env.AUTH_GOOGLE_SECRET ?? "",
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   callbacks: {
